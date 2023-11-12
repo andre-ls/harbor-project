644ce9c0dec672411cb2504a4ca74b51f75c1ceb
andre-ls <andre.luis.e3@gmail.com>
2023-11-12 10:21:49 -0300

    Change Timezone

--- settings.py
+++ settings.py
@@ -1,9 +1,10 @@
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
 def init():
     global EXTRACT_TIME
     global FILE_PATH
     global FILE_NAME
-    EXTRACT_TIME = datetime.now()
+    EXTRACT_TIME = datetime.now(ZoneInfo("America/Sao_Paulo"))
     FILE_PATH = EXTRACT_TIME.strftime('%Y-%m-%d')
     FILE_NAME = EXTRACT_TIME.strftime('%H:%M:%S')
