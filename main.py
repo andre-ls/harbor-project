8470a16f27bf0fc997af869aa9c9ab7787c7e773
andre-ls <andre.luis.e3@gmail.com>
2023-11-14 14:15:51 -0300

    Removed Views Generation from Code

--- main.py
+++ main.py
@@ -2,40 +2,28 @@ import functions_framework
 import settings
 from extractions import docked_ships_extract, operations_extract
 from transformations import docked_ships_transform, operations_transform
-from views import dock_view, operations_view
 
-DATALAKE_PATH = 'gcs://harbor-datalake'
+#DATALAKE_PATH = 'gcs://harbor-datalake'
+DATALAKE_PATH = '/harbor-datalake'
 
 def dockedShipsPipeline():
     rawPath = DATALAKE_PATH + '/raw/docked_ships'
     trustedPath = DATALAKE_PATH + '/trusted/docked_ships'
 
     savedRawFile = docked_ships_extract.run(rawPath)
-    savedTrustedFile = docked_ships_transform.run(savedRawFile,trustedPath)
-    return savedTrustedFile
+    docked_ships_transform.run(savedRawFile,trustedPath)
 
 def operationsPipeline():
     rawPath = DATALAKE_PATH + '/raw/operations'
     trustedPath = DATALAKE_PATH + '/trusted/operations'
 
     savedRawFile = operations_extract.run(rawPath)
-    savedTrustedFile = operations_transform.run(savedRawFile,trustedPath)
-    return savedTrustedFile
-
-def viewsPipeline(dockedShipsTrustedFile, operationsTrustedFile):
-
-    dockViewPath = DATALAKE_PATH + '/views/dock'
-    operationsViewPath = DATALAKE_PATH + '/views/operations'
-
-    dock_view.generateView(dockedShipsTrustedFile,operationsTrustedFile,dockViewPath)
-    operations_view.generateView(dockedShipsTrustedFile,operationsTrustedFile,operationsViewPath)
+    operations_transform.run(savedRawFile,trustedPath)
 
 def runPipeline():
     settings.init()
-
-    dockedShipsTrustedFile = dockedShipsPipeline()
-    operationsTrustedFile = operationsPipeline()
-    viewsPipeline(dockedShipsTrustedFile,operationsTrustedFile)
+    dockedShipsPipeline()
+    operationsPipeline()
 
 @functions_framework.http
 def cloudEntrypoint(request):
